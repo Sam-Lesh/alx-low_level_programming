146449abbe7055e74c134470229160908e6fae48
Samuel Eleshinnla <samelesh1@gmail.com>
2023-02-18 10:55:04 -0800

    I edited task 3 to have integer j

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,19 @@
  */
 int main(void)
 {
-	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char ALP[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i, j;
 
-	for (i = 0; i < 52; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
+	for (j = 0; j < 26; j++);
+	{
+		putchar(ALP[j]);
+	}
+
 	putchar('\n');
 	return (0);
 }

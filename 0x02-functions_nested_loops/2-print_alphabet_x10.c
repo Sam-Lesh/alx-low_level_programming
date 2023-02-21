eed9a87277111aa12f24972100f359fc7f9b2a05
Samuel Eleshinnla <samelesh1@gmail.com>
2023-02-21 12:01:34 -0800

    First commit to task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10x the alphabet in lower case
+ *
+ * Return: Always 0
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	int j;
+
+	while (i < 10)
+	{
+		for (j = 97; j < 123; j++)
+		{
+			_putchar(j);
+		}
+		i++;
+	}
+	_putchar('\n');
+}

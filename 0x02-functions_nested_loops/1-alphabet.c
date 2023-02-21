85fb0ead94daecf424adbcbcbd21aefa70d9703a
Samuel Eleshinnla <samelesh1@gmail.com>
2023-02-21 11:48:06 -0800

    First commit for task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lower case
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}

d923da6e8d4e135ff031824dcdebd709e921c775
Samuel Eleshinnla <samelesh1@gmail.com>
2023-02-21 10:53:53 -0800

    Finally had a breakthrough with Betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * Main - Prints _putchar
+ * main - Prints _putchar as a message
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
+
 {
 	int arr[8] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int i;

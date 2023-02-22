a048e0bc56941e062e969e142e043cd2a120cf46
Samuel Eleshinnla <samelesh1@gmail.com>
2023-02-21 18:58:40 -0800

    First commit to task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - This program checks for both upper and lower case alphabets
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: returns 1 for alphabets and 0 for others
+ *
+ */
+
+int _isalpha(int c)
+{
+	if (c > 64 && c < 91 || c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}

93c29abee40a458e620234c9de2c20a275de85af
Samuel Eleshinnla <samelesh1@gmail.com>
2023-02-22 01:24:35 -0800

    Finally fixed Betty for task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,15 +11,15 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 48; a < 51; a++)
+	for (a = 48; a <= 50; a++)
 	{
-		for (b = 48; b < 58; b++)
+		for (b = 48; b <= 57; b++)
 		{
-			for (c = 48; c < 54; c++)
+			for (c = 48; c <= 53; c++)
 			{
-				for (d = 48; d < 58; d++)
+				for (d = 48; d <= 57; d++)
 				{
-					if (a > 50 && b > 52)
+					if (a >= 50 && b >= 52)
 						break;
 					_putchar(a);
 					_putchar(b);

b03c413c80fbb0ce4866737a358d69c2e34de200
Samuel Eleshinnla <samelesh1@gmail.com>
2023-02-21 20:23:56 -0800

    First commit to task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute in a day
+ *
+ * Return: no return
+ *
+ */
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a < 51; a++)
+	{
+		for (b = 48; b < 58; b++)
+		{
+			for (c = 48; c < 54; c++)
+			{
+				for (d = 48; d < 58; d++)
+				{
+					if (a > 51 && b > 53)
+						break;
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}

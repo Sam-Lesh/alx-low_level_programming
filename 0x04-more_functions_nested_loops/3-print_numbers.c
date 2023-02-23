a353d26405c1bf3580535c5ec27857a9c7ac2ee9
Samuel Eleshinnla <samelesh1@gmail.com>
2023-02-23 08:39:08 -0800

    First commit to task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers 0 to 9
+ *
+ * Return: no return
+ *
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}

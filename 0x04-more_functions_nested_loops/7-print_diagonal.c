d1c2a09d94759b566082168b77e15c9ffbad3e8c
Samuel Eleshinnla <samelesh1@gmail.com>
2023-02-23 10:59:25 -0800

    I added appropriate spaces on the next lines

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,12 +11,16 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, a;
 
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
+			for (a = 0; a < i; a++)
+			{
+				_putchar(32);
+			}
 			_putchar(92);
 			_putchar('\n');
 		}

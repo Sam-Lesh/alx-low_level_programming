63ba730cd1ee8074628b8584de11baf43f969a93
Samuel Eleshinnla <samelesh1@gmail.com>
2023-02-23 08:20:14 -0800

    First commit to taskk 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first character
+ * @b: second character
+ *
+ * Return: returns 0
+ *
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}

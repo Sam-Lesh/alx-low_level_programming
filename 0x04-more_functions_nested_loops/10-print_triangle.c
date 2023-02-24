fa5358bea4a9e6140b67c93591ae978c30db11de
Samuel Eleshinnla <samelesh1@gmail.com>
2023-02-24 02:55:10 -0800

    First commit to task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ *
+ * @size: size of the triangle
+ *
+ * Return: no return
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i, m, l;
+	int j = size;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (l = 1; l < j; l++)
+			{
+				_putchar(32);
+			}
+			for (m = j; m < size + 1; m++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+			j--;
+		}
+	}
+	else
+	{
+		_putchar('\n')
+	}
+}
+

ad9a0f017c5f3e5f1f3eac5ab1110cce91c86319
Samuel Eleshinnla <samelesh1@gmail.com>
2023-02-27 11:59:20 -0800

    First commit to task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates it to 98
+ *
+ * @n: integer to be updated
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

0aec0ca75cef64e7910bee9908380d4d2da04f4c
Samuel Eleshinnla <samelesh1@gmail.com>
2023-02-28 08:41:26 -0800

    I introduced another array

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,14 +13,15 @@ void rev_string(char *s)
 	int c = 0;
 	int a;
 	int b;
+	int z[];
 
 	while (*(s + c) != '\0')
 		c++;
 
 	for (a = c - 1; a >= 0; a--)
 	{
-		*(s + b) = (s[a]);
+		*(z + b) = (s[a]);
 		b++;
 	}
-	return (s);
+	return (z);
 }

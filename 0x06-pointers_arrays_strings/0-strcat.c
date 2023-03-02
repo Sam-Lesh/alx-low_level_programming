9db950cc6ac3b09338fcae4be21e32be1b51f708
Samuel Eleshinnla <samelesh1@gmail.com>
2023-03-02 09:19:25 -0800

    I used * again

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,18 +11,16 @@ char *_strcat(char *dest, char *src)
 {
 	int a = 0;
 	int b = 0;
-	int e = 0;
+	int e;
 
 	while (*(dest + a) != '\0')
 		a++;
 	while (*(src + b) != '\0')
 		b++;
 
-	int c = (a + b);
-
 	for (e = 0; e < b; e++)
 	{
-		dest[a + e] = src[e];
+		*(dest + a + e) = *(src + e);
 	}
 	return (dest);
 }

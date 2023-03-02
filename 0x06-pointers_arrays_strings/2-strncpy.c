f1e78d17ac919d65788d024eff8d5b3db1589f70
Samuel Eleshinnla <samelesh1@gmail.com>
2023-03-02 12:35:12 -0800

    First commit to task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies the string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes
+ * Return: return copied
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[a] = src[a];
+	for ( ; a < n; a++)
+		dest[i] = '\0';
+	return (dest);
+}

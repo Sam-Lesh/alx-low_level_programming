78640d66f899a192ff2646338684ab5507719b78
Samuel Eleshinnla <samelesh1@gmail.com>
2023-03-02 10:47:44 -0800

    First commit to task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: string
+ * @src: string
+ * @n: integer
+ *
+ * Return: Always 0
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+	int e = 0;
+
+	while (*(dest + a) != '\0')
+		a++;
+	for (e = 0; e < n; e++)
+	{
+		*(dest + a + e) = *(src + e);
+	}
+	return (dest);
+}

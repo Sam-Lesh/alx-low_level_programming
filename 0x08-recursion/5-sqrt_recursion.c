63d53356789921e7d34a1f52de05988e1d9f2ec4
Samuel Eleshinnla <samelesh1@gmail.com>
2023-03-08 16:05:35 -0800

    First commit to task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * check - checks if n is a square
+ * @n: input integer
+ * @i: counter
+ * Return: returns sqrt or -1
+ */
+
+int check(int n, int i)
+{
+	if ((n / i) == i)
+	{
+		return (i);
+	}
+	else
+		return (-1);
+	return (0 + check(n, (i + 1)));
+}
+
+/**
+ * _sqrt_recursion - returns sqrt
+ * @n: input integer
+ * Return: returns sqrt or -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n > 0)
+		return (check(n, 1));
+}

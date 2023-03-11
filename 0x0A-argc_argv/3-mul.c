e5779336c452117587313f633abf791fae6da690
Samuel Eleshinnla <samelesh1@gmail.com>
2023-03-11 03:06:28 -0800

    First commit to task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: no return
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc > 2)
+	{
+		mul = (atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (1);
+}

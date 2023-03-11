a1e28f7e83b7193f2caf848ac8385bbfab359444
Samuel Eleshinnla <samelesh1@gmail.com>
2023-03-11 03:23:08 -0800

    Fixing file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,14 +12,12 @@ int main(int argc, char *argv[])
 {
 	int mul;
 
-	if (argc > 2)
-	{
-		mul = (atoi(argv[1]) * atoi(argv[2]));
-		printf("%d\n", mul);
-	}
-	else
+	if (argc < 3)
 	{
 		printf("Error\n");
+		return (1);
 	}
-	return (1);
+	mul = (atoi(argv[1]) * atoi(argv[2]));
+	printf("%d\n", mul);
+	return (0);
 }

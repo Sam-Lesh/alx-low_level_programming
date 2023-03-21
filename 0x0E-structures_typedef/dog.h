8020b61341db07add3eaf37c15a0e11bbb53aa11
Samuel Eleshinnla <samelesh1@gmail.com>
2023-03-20 19:08:28 -0700

    First commit to tas 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef _DOG_
 #define _DOG_
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 /**
  * struct dog - short description
  * @name: name of dog
@@ -9,12 +15,14 @@
  * Description: Longer description
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog;
+
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

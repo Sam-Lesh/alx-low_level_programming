eb7435029d2890ca373e58f0c3fe2e849944316b
Samuel Eleshinnla <samelesh1@gmail.com>
2023-03-20 18:33:50 -0700

    First commit to task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_
+#define _DOG_
+/**
+ * struct dog - short description
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: Longer description
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif

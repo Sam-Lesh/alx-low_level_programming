91f4528fd4aa7275ee15888980f6812d757c1634
Samuel Eleshinnla <samelesh1@gmail.com>
2023-03-22 08:34:33 -0700

    First commit to task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - functions searches for an integer
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: function pointer
+ * Return: Always 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int j = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (j);
+			j++;
+		}
+	}
+	return (-1);
+}

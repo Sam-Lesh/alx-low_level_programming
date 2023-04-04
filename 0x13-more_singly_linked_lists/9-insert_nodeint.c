a6b5862736b9f7052eceaef19e875d3461f3e444
Samuel Eleshinnla <samelesh1@gmail.com>
2023-04-04 07:26:03 -0700

    Updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp = *head;
 	if (idx != 0)
 	{
-		for (i = 1; i < idx && temp->next != NULL; i++)
+		for (i = 1; i < idx && temp != NULL; i++)
 		{
 			temp = temp->next;
 		}

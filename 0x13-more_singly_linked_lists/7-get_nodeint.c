8824019761600b12975bd740ccefca8425e4b6ad
Samuel Eleshinnla <samelesh1@gmail.com>
2023-04-04 04:52:25 -0700

    I considered if the node does not exist, return NULL

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,19 +9,14 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *temp;
 	unsigned int i;
 
 	if (head == NULL)
 		return (NULL);
 
-	temp = head;
-
-	for (i = 0; i < index; i++)
+	for (i = 0; i < index && head != NULL; i++)
 	{
-		temp = temp->next;
+		head = head->next;
 	}
-	if (temp == NULL)
-		return (NULL);
-	return (temp);
+	return (head);
 }
